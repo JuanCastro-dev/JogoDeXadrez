4d965ae95666ad54ecbed8341aada3fa709f62bb
Juan <juancmendonca@gmail.com>
2024-12-29 14:24:18 -0300

    Pacote xadrez e impressão do tabuleiro adicionados

---
+++ JogoDeXadrez/src/Xadrez/PecaDeXadrez.java
@@ -0,0 +1,18 @@
+package Xadrez;
+
+import Tabuleiro.posicao.Peca;
+import Tabuleiro.posicao.Tabuleiro;
+
+public class PecaDeXadrez extends Peca {
+
+    private Cor cor;
+
+    public PecaDeXadrez(Tabuleiro tabuleiro, Cor cor) {
+        super(tabuleiro);
+        this.cor = cor;
+    }
+
+    public Cor getCor() {
+        return cor;
+    }
+}
--- JogoDeXadrez/src/aplicacao/Program.java
+++ JogoDeXadrez/src/aplicacao/Program.java
@@ -2,7 +2,11 @@ package aplicacao;
 
 import Tabuleiro.posicao.Posicao;
 import Tabuleiro.posicao.Tabuleiro;
+import Xadrez.PartidaDeXadrez;
 
 public class Program {
-    
+    public static void main(String[] args) {
+        PartidaDeXadrez partida = new PartidaDeXadrez();
+        UI.printTabuleiro(partida.getPecasDeXadrez());
+    }
 }
